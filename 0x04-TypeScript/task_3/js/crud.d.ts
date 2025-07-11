7a34aa212931875cedfa6eabc4faa680c4f70e58
Halima Muktar <halimamuk1307@gmail.com>
2025-07-12 02:10:10 +0300

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,17 +1,7 @@
+/// <reference path="./interface.ts" />
+
 import { RowID, RowElement } from './interface';
-import * as CRUD from './crud';
 
-const row: RowElement = {
-  firstName: "Guillaume",
-  lastName: "Salva"
-};
-
-const newRowID: RowID = CRUD.insertRow(row);
-
-const updatedRow: RowElement = {
-  ...row,
-  age: 23
-};
-
-CRUD.updateRow(newRowID, updatedRow);
-CRUD.deleteRow(newRowID);
+export function insertRow(row: RowElement): RowID;
+export function deleteRow(rowId: RowID): void;
+export function updateRow(rowId: RowID, row: RowElement): RowID;
