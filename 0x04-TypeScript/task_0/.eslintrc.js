90aa34b340ca97ad43bbe337fb25891a8cdc461c
Halima Muktar <halimamuk1307@gmail.com>
2025-07-11 14:08:47 +0300

    Create .eslintrc.js

---
+++ 0x04-TypeScript/task_0/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    // You can customize rules here
+  },
+};
