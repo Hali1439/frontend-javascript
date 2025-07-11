4024f58dd24f98c253dfc1d0db759cbd99147549
Halima Muktar <halimamuk1307@gmail.com>
2025-07-12 01:12:47 +0300

    Create main.ts

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,20 @@
+// Define Teacher interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // allow additional properties
+}
+
+// Create a Teacher object with extra property
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
