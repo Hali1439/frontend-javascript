5e971370719ea775de03998694a15c430fd4852e
Halima Muktar <halimamuk1307@gmail.com>
2025-07-13 02:56:04 +0300

    Create Java.d.ts

---
+++ 0x04-TypeScript/task_4/dist/subjects/Java.d.ts
@@ -0,0 +1,11 @@
+/// <reference path="Teacher.d.ts" />
+/// <reference path="Subject.d.ts" />
+declare namespace Subjects {
+    interface Teacher {
+        experienceTeachingJava?: number;
+    }
+    class Java extends Subject {
+        getRequirements(): string;
+        getAvailableTeacher(): string;
+    }
+}
