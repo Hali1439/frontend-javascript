d2783bd32d6a14dfe9e7edd7905382a6be3679f5
Halima Muktar <halimamuk1307@gmail.com>
2025-07-13 01:30:53 +0300

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,38 +1,18 @@
 // Define Teacher interface
-interface Teacher {
+export interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
   location: string;
+  yearsOfExperience?: number;
   [key: string]: any; // allow additional properties
 }
 
 // Create a Teacher object with extra property
 const teacher3: Teacher = {
   firstName: 'John',
-  fullTimeEmployee: false,
   lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-
-console.log(teacher3);
-// Define Teacher interface
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
-}
-
-// Example usage of Teacher
-const teacher3: Teacher = {
-  firstName: 'John',
   fullTimeEmployee: false,
-  lastName: 'Doe',
   location: 'London',
   contract: false,
 };
@@ -40,7 +20,7 @@ const teacher3: Teacher = {
 console.log(teacher3);
 
 // Extend Teacher to create Directors interface
-interface Directors extends Teacher {
+export interface Directors extends Teacher {
   numberOfReports: number;
 }
 
@@ -56,31 +36,30 @@ const director1: Directors = {
 console.log(director1);
 
 // Interface for printTeacher function
-interface printTeacherFunction {
+export interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
 // Implementing the function
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+export const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
-// Example usage
 console.log(printTeacher("John", "Doe")); // Output: J. Doe
 
 // Interface for the StudentClass constructor
-interface StudentConstructor {
+export interface StudentConstructor {
   new(firstName: string, lastName: string): StudentInterface;
 }
 
 // Interface describing the class
-interface StudentInterface {
+export interface StudentInterface {
   workOnHomework(): string;
   displayName(): string;
 }
 
 // Implementing the class
-class StudentClass implements StudentInterface {
+export class StudentClass implements StudentInterface {
   constructor(private firstName: string, private lastName: string) {}
 
   workOnHomework(): string {
