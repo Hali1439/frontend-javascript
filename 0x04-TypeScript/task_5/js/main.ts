beb10b554dfe82d8150be5065ff47e89080269ea
Halima Muktar <halimamuk1307@gmail.com>
2025-07-13 03:08:25 +0300

    Update main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,24 +1,17 @@
 export interface MajorCredits {
+  _majorCreditBrand: void;
   credits: number;
-  _majorCreditBrand: void; // branding to make it nominal
 }
 
 export interface MinorCredits {
+  _minorCreditBrand: void;
   credits: number;
-  _minorCreditBrand: void; // branding to make it nominal
 }
 
 export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-  return {
-    credits: subject1.credits + subject2.credits,
-    _majorCreditBrand: undefined
-  };
+  return { credits: subject1.credits + subject2.credits } as MajorCredits;
 }
 
 export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
-  return {
-    credits: subject1.credits + subject2.credits,
-    _minorCreditBrand: undefined
-  };
+  return { credits: subject1.credits + subject2.credits } as MinorCredits;
 }
-
